db46e737f6fae103da77923b18153a791c50e642
AshishThakur <at0748307@gmail.com>
2026-09-17 20:46:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const btn = document.getElementById('menu-btn');
+  const menu = document.getElementById('mobile-menu');
+  const line1 = document.getElementById('line-1');
+  const line2 = document.getElementById('line-2');
+  const line3 = document.getElementById('line-3');
+
+  let isOpen = false;
+
+  btn.addEventListener('click', () => {
+    isOpen = !isOpen;
+
+    if (isOpen) {
+      menu.style.maxHeight = menu.scrollHeight + 'px';
+      menu.classList.add('border-gray-800');
+
+      // Cross animation
+      line1.classList.add('rotate-45', 'translate-y-2');
+      line2.classList.add('opacity-0');
+      line3.classList.add('-rotate-45', '-translate-y-2');
+    } else {
+      menu.style.maxHeight = '0px';
+      setTimeout(() => menu.classList.remove('border-gray-800'), 300);
+
+      // Reset to hamburger
+      line1.classList.remove('rotate-45', 'translate-y-2');
+      line2.classList.remove('opacity-0');
+      line3.classList.remove('-rotate-45', '-translate-y-2');
+    }
+  });
+
+  
